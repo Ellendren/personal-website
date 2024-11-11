f3508dde71adfc452d9bf4ea685b4c45c4b032fe
Ellendren <lukesh@sonic.net>
2024-11-11 10:24:45 -0800

    added edu section to it page

--- serv/index.js
+++ serv/index.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const fs = require("fs");
 const http = require("http");
-const https = require("https");
 
-dotenv.config();
-
-let options = {
-    key: fs.readFileSync(process.env.KEY),
-    cert: fs.readFileSync(process.env.CERT)
-}
-let port = process.env.PORT;
-let tartget = process.env.TARGET;
+let port = 3000;
+let tartget = '../frontend';
 
 let app = express();
 app.use(express.static(tartget));
 
 http.createServer(app).listen(port);
-// https.createServer(options, app).listen(port);
